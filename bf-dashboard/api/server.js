3220dc20e806998b9da9eeb2c211ee5dcf806834
Mathis DI MASCIO <mathis.dimascio@gmail.com>
2023-10-18 12:49:21 +0200

    upload OK

--- bf-dashboard/api/server.js
+++ bf-dashboard/api/server.js
@@ -2,14 +2,17 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 import cors from 'cors';
+import multer from 'multer';
 
 const __dirname = path.resolve();
 const app = express();
 const port = 5174; // Choose a port for your server
-const folderPath = path.join(__dirname, "images");
+const folderPath = path.join(__dirname, 'images');
+const allowedExtensions = ['.jpg', '.jpeg', '.png']; // Add the extensions you want to allow
 
 app.use(cors());
 
+// Get a list of folders in the images directory
 app.get('/api/folders', (req, res) => {
   fs.readdir(folderPath, (err, files) => {
     if (err) {
@@ -21,9 +24,9 @@ app.get('/api/folders', (req, res) => {
   });
 });
 
+// Get a list of images in a specific folder
 app.get('/api/images/:folderName', (req, res) => {
   const folderName = req.params.folderName;
-  const validExtensions = ['.jpg', '.jpeg', '.png']; // Add the extensions you want to allow
   const images = [];
 
   fs.readdir(path.join(folderPath, folderName), (err, files) => {
@@ -33,9 +36,9 @@ app.get('/api/images/:folderName', (req, res) => {
     } else {
       files.forEach(file => {
         // Extract the file extension
-        const fileExtension = file.slice(((file.lastIndexOf(".") - 1) >>> 0) + 2);
+        const fileExtension = file.slice(((file.lastIndexOf('.') - 1) >>> 0) + 2);
 
-        if (validExtensions.includes(`.${fileExtension.toLowerCase()}`)) {
+        if (allowedExtensions.includes(`.${fileExtension.toLowerCase()}`)) {
           images.push(file);
         }
       });
@@ -67,6 +70,59 @@ app.get('/api/images/:folderName/:imageName', (req, res) => {
       fs.createReadStream(imagePath).pipe(res);
     }
   });
+});
+
+// Add a route to create a folder with the current date
+app.post('/api/createFolder', (req, res) => {
+  const currentDate = new Date().toISOString().slice(0, 10); // Get the current date in 'YYYY-MM-DD' format
+  const folderPath = path.join(__dirname, 'images', currentDate);
+
+  try {
+    fs.mkdirSync(folderPath, { recursive: true });
+    res.json({ message: 'Folder created', folderPath });
+  } catch (error) {
+    console.error('Error creating folder:', error);
+    res.status(500).json({ error: 'Server Error' });
+  }
+});
+
+// Set up multer for handling image uploads
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // Create a folder with the current date for each image upload
+    const currentDate = new Date().toISOString().slice(0, 10); // Get the current date in 'YYYY-MM-DD' format
+    const folderPath = path.join(__dirname, 'images', currentDate);
+
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath, { recursive: true });
+    }
+    
+    cb(null, folderPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+
+const fileFilter = (req, file, cb) => {
+  const extension = path.extname(file.originalname).toLowerCase();
+  if (allowedExtensions.includes(extension)) {
+    return cb(null, true);
+  }
+  cb(new Error('Invalid file extension. Allowed extensions are .jpg, .jpeg, and .png'));
+};
+
+const upload = multer({ storage, fileFilter });
+
+// Upload multiple images to the 'uploads' folder
+app.post('/api/uploadImages', upload.array('images', 10), (req, res) => {
+  if (req.fileValidationError) {
+    return res.status(400).json({ error: req.fileValidationError.message });
+  }
+
+  const uploadedImages = req.files.map(file => file.originalname);
+  res.json({ uploadedImages });
 });
 
 app.listen(port, () => {
